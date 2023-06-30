631cf685e97cbff2a688690b0c450d0894376313
JacksonChi <51813740+JacksonChiy@users.noreply.github.com>
2023-06-30 18:49:22 +0100

    refactor code, change speed value, fix bug in check_next_reached

--- muti_robot_navigation_2.py
+++ muti_robot_navigation_2.py
@@ -35,16 +35,14 @@ def check_next_reached(agents, schedule, index):
     for i in agents:
         pos = p.getBasePositionAndOrientation(i)[0]
         if(index >= len(schedule[i])):
-            return True
+            continue
         if (not checkPosWithBias(pos, [schedule[i][index]["x"], schedule[i][index]["y"]], 0.2)):
             return False
     return True
 
 def set_velocity(agent, schedule, index):
-    speed = 10
-    direct = False
+    speed = 20
     forward = 0
-    turn = 0
     basePos = p.getBasePositionAndOrientation(agent)
     x = basePos[0][0]
     y = basePos[0][1]
@@ -64,17 +62,12 @@ def set_velocity(agent, schedule, index):
 
     print(Orientation[2], goal_direct)
 
-
     if (abs(Orientation[2] - goal_direct) <= 0.2):
         turn = 0
         direct = True
-    elif (Orientation[2] < goal_direct):
+    else:
         forward = 0
-        turn = 0.1
-        direct = False
-    elif (Orientation[2] > goal_direct):
-        forward = 0
-        turn = -0.1
+        turn = goal_direct - Orientation[2]
         direct = False
 
     if (direct is True):
@@ -85,98 +78,82 @@ def set_velocity(agent, schedule, index):
         forward = 0
         turn = 0
 
+    if(turn != 0):
+        speed = 5
     rightWheelVelocity = (forward + turn) * speed
     leftWheelVelocity = (forward - turn) * speed
     return leftWheelVelocity, rightWheelVelocity
 
+
+def read_input(yaml_file, env_loaded):
+    agents = []
+    goals = {}
+    with open(yaml_file, 'r') as param_file:
+        try:
+            param = yaml.load(param_file, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+        if(env_loaded is True):
+            for i in param["agents"]:
+                goals[i["name"]] = i["goal"]
+            return None, goals, True
+        for i in param["agents"]:
+            startPosition = (i["start"][0], i["start"][1], 0)
+            boxId = p.loadURDF("data/turtlebot.urdf", startPosition, startOrientation, globalScaling=1)
+            agents.append(boxId)
+            goals[boxId] = i["goal"]
+        dimensions = param["map"]["dimensions"]
+        p.resetDebugVisualizerCamera(cameraDistance=dimensions[0] * 1, cameraYaw=0, cameraPitch=-89,
+                                     cameraTargetPosition=[dimensions[0] / 2, dimensions[1] / 2, 0])
+
+        createBoundaries(dimensions[0], dimensions[1])
+        if env_loaded is False:
+            for i in param["map"]["obstacles"]:
+                p.loadURDF("cube.urdf", [i[0], i[1], 0.5])
+    return agents, goals, True
+
+def read_output(output_yaml_file):
+    with open(output_yaml_file, 'r') as param_file:
+        try:
+            param = yaml.load(param_file, Loader=yaml.FullLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return param["schedule"]
+
+def navigation(agents, goals, schedule):
+    index = 0
+    while (not check_goal_reached(agents, goals)):
+        time.sleep(1. / 240.)
+
+
+        for i in agents:
+            leftWheelVelocity, rightWheelVelocity = set_velocity(i, schedule, index)
+            print(i, leftWheelVelocity, rightWheelVelocity)
+            p.setJointMotorControl2(i, 0, p.VELOCITY_CONTROL, targetVelocity=leftWheelVelocity, force=1000)
+            p.setJointMotorControl2(i, 1, p.VELOCITY_CONTROL, targetVelocity=rightWheelVelocity, force=1000)
+        if(check_next_reached(agents, schedule, index)):
+            index+=1
+
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
-startOrientation = p.getQuaternionFromEuler([0,0,0])
-agents = []
-goals = {}
-
-with open("input2.yaml", 'r') as param_file:
-    try:
-        param = yaml.load(param_file, Loader=yaml.FullLoader)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-    for i in param["agents"]:
-        startPosition = (i["start"][0], i["start"][1], 0)
-        boxId = p.loadURDF("data/turtlebot.urdf", startPosition, startOrientation, globalScaling=1)
-        agents.append(boxId)
-        goals[boxId] = i["goal"]
-    dimensions = param["map"]["dimensions"]
-    p.resetDebugVisualizerCamera(cameraDistance=dimensions[0] * 1.2, cameraYaw=0, cameraPitch=-89,
-                                 cameraTargetPosition=[dimensions[0] / 2 -0.5, dimensions[1] / 2-0.5, 0])
-
-    createBoundaries(dimensions[0],dimensions[1])
-
-    for i in param["map"]["obstacles"]:
-        p.loadURDF("cube.urdf", [i[0], i[1], 0.5])
-
-cbs.main("input2.yaml", "output.yaml")
-
-with open("output.yaml", 'r') as param_file:
-    try:
-        param = yaml.load(param_file, Loader=yaml.FullLoader)
-    except yaml.YAMLError as exc:
-        print(exc)
-
-schedule = param["schedule"]
 p.setRealTimeSimulation(1)
-index = 0
 p.setGravity(0, 0, -10)
-while (not check_goal_reached(agents, goals)):
-    time.sleep(1. / 240.)
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+global env_loaded
+env_loaded = False
+agents, goals, env_loaded = read_input("scene/room_scene_4_bots.yaml", env_loaded)
+cbs.main("scene/room_scene_4_bots.yaml", "output.yaml")
+schedule = read_output("output.yaml")
+navigation(agents, goals, schedule)
 
-    for i in agents:
-        print(agents)
-        print(schedule)
-        print(i)
-        print(index)
-        leftWheelVelocity, rightWheelVelocity = set_velocity(i, schedule, index)
-        print(i, leftWheelVelocity, rightWheelVelocity)
-        p.setJointMotorControl2(i, 0, p.VELOCITY_CONTROL, targetVelocity=leftWheelVelocity, force=1000)
-        p.setJointMotorControl2(i, 1, p.VELOCITY_CONTROL, targetVelocity=rightWheelVelocity, force=1000)
-    if(check_next_reached(agents, schedule, index)):
-        index+=1
-
-print("stage 1 finished")
-
-for i in agents:
-    pos = [p.getBasePositionAndOrientation(i)[0][0], p.getBasePositionAndOrientation(i)[0][1], 2]
-    p.loadURDF("sphere_small.urdf", pos)
+# _, goals, env_loaded = read_input("input3.yaml", env_loaded)
+# cbs.main("input3.yaml", "output.yaml")
+# schedule = read_output("output.yaml")
+# navigation(agents, goals, schedule)
 
 
-with open("input3.yaml", 'r') as param_file:
-    try:
-        param = yaml.load(param_file, Loader=yaml.FullLoader)
-        for i in param["agents"]:
-            goals[boxId] = i["goal"]
-    except yaml.YAMLError as exc:
-        print(exc)
-cbs.main("input3.yaml", "output.yaml")
 
-with open("output.yaml", 'r') as param_file:
-    try:
-        param = yaml.load(param_file, Loader=yaml.FullLoader)
-    except yaml.YAMLError as exc:
-        print(exc)
 
-schedule = param["schedule"]
-print(schedule)
 
-# p.setRealTimeSimulation(1)
-index = 0
-# p.setGravity(0, 0, -10)
-while (not check_goal_reached(agents, goals)):
-    time.sleep(1. / 240.)
-    for i in agents:
-        leftWheelVelocity, rightWheelVelocity = set_velocity(i, schedule, index)
-        print(i, leftWheelVelocity, rightWheelVelocity)
-        p.setJointMotorControl2(i, 0, p.VELOCITY_CONTROL, targetVelocity=leftWheelVelocity, force=1000)
-        p.setJointMotorControl2(i, 1, p.VELOCITY_CONTROL, targetVelocity=rightWheelVelocity, force=1000)
-    if(check_next_reached(agents, schedule, index)):
-        index+=1
+
